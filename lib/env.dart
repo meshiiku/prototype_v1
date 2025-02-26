1c90f7eaa31802fce9c741f55e63f30dd60df896
oto <ottotak@outlook.com>
2025-02-26 18:00:28 +0900

    Revert "feature: ホットペッパーのAPI"

--- lib/env.dart
+++
@@ -1,9 +0,0 @@
-import 'package:envied/envied.dart';
-
-part "env.g.dart";
-
-@Envied(path: '.env')
-abstract class Env {
-  @EnviedField(varName: 'HOTPEPPER_API_KEY', obfuscate: true)
-  static String apiKey = _Env.apiKey;
-}
--- lib/screen/mypage-screen.dart
+++ lib/screen/mypage-screen.dart
@@ -1,6 +1,5 @@
 import "package:flutter/material.dart";
 import "package:prototype_v1/components/user_card.dart";
-import "package:prototype_v1/env.dart";
 import "package:prototype_v1/model/user_profile.dart";
 
 class MyPageScreen extends StatelessWidget {
@@ -11,6 +10,7 @@ class MyPageScreen extends StatelessWidget {
     // TODO: implement build
     return ListView(
       children: [
+        Text("マイページ"),
         UserCard(profile: UserProfile("username", ["ラーメン"])),
       ],
     );
