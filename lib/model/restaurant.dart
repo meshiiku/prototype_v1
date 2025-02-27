f83cd56f2d7e3266b88d3a68d3e6ec1cb25aed0f
oto <ottotak@outlook.com>
2025-02-27 21:54:55 +0900

    improve: レストランクラスのプロパティにジャンルやIDなどを追加

--- lib/model/restaurant.dart
+++ lib/model/restaurant.dart
@@ -2,13 +2,21 @@ class Restaurant {
   final String name;
   final double lat;
   final double lng;
+  final String id;
+  String? open;
   String? logoImage;
+  String? genre;
+  String? catchMessage;
 
   Restaurant({
     required this.name,
     required this.lat,
     required this.lng,
+    required this.id,
     this.logoImage,
+    this.open,
+    this.genre,
+    this.catchMessage,
   });
 
   factory Restaurant.fromJson(Map<String, dynamic> json) {
@@ -17,6 +25,10 @@ class Restaurant {
       lat: (json['lat']),
       lng: (json['lng']),
       logoImage: json["logo_image"],
+      id: json["id"],
+      open: json["open"],
+      genre: json["genre"]["name"],
+      catchMessage: json["genre"]["catch"],
     );
   }
 }
