7cb090842ec489bf7ad3dc07b8c1e2acea5a7395
oto <ottotak@outlook.com>
2025-02-27 19:05:34 +0900

    chore: 端末に保存されたjwtをリセットするメソッドの追加

--- lib/saves/jwt.dart
+++ lib/saves/jwt.dart
@@ -6,6 +6,11 @@ Future<void> saveJWTToken(String token) async {
   await prefs.setString("jwt_token", token);
 }
 
+Future<void> resetJWTToken() async {
+  final prefs = await SharedPreferences.getInstance();
+  await prefs.remove("jwt_token");
+}
+
 // JWTトークンを取得する
 Future<String?> getJWTToken() async {
   final prefs = await SharedPreferences.getInstance();
